a51cb19e3c411c15cc3edddc6f140db15589460f
EnDiGi <degiorgienrico.26042012@gmail.com>
2024-11-18 22:03:58 +0100

    Finally finished the grid.
    It took WAY too long...

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const players = ["X", "O"]
 let curr_player = players[Math.random() <= 0.5 ? 0 : 1]
+let turn = curr_player === "X" ? 0 : 1
 
 document.getElementById("turn_info").textContent = `${curr_player}'s turn`
 
@@ -8,3 +9,18 @@ function get_current_diff(){
 	return document.querySelector('input[name = "difficulty"]:checked').value
 }
 
+function make_move(pos_number){
+	button = Array.from(document.getElementsByClassName("button"))[pos_number]
+
+	if(button.textContent !== " "){
+		return
+	}
+
+	button.style.color = curr_player === "X" ? "red" : "blue"
+	button.textContent = curr_player
+
+	
+	turn += 1
+	curr_player = players[turn % 2]	
+	document.getElementById("turn_info").textContent = `${curr_player}'s turn`
+}
